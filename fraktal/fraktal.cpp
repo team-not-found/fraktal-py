e13237bc220c7f29e24f4061dbc6ce40ad9a56dc
emil <varadi.emil3@gmail.com>
2021-03-27 20:52:27 +0200

    removed from cache

--- fraktal/fraktal.cpp
+++
@@ -1,7 +0,0 @@
-#include "fraktal.hpp"
-
-#include<iostream>
-
-void Fraktal::hello() {
-    std::cout << "Hello World!" << std::endl;
-}
--- fraktal/fraktal.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class Fraktal {
-public:
-    void hello();
-};
